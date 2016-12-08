c140588cb1910495c0dfaf1dfa58266780ab0241
Anna Dolan <anna.dolan@gmail.com>
2016-12-08 09:27:04 -0700

    Display feed on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @user = Profile.find_user(current_user.username)
     @repos = Repo.by_user(current_user.token)
+    @feed = Event.performed_by_user(current_user.token, current_user.username)
   end
 end
